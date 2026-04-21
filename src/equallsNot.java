5efa4aa5b8c9f0f775b6a80719824c0bfbc8be3c
alzahara <shahdaljabri33@gmail.com>
2026-04-21 16:11:34 +0400

    searching for the word is

---
+++ src/equallsNot.java
@@ -0,0 +1,18 @@
+public class equallsNot {
+    public static void main(String[] args) {
+        System.out.println(equalIsNot("This is not"));
+        System.out.println(equalIsNot("This is notnot"));
+    }
+
+    public static boolean equalIsNot(String text) {
+        int isCount = 0;
+        int notCount = 0;
+
+        for (int i = 0; i <= text.length() - 2; i++) {
+            if (text.substring(i, i + 2).equals("is")) {
+                isCount++;
+            }
+        }
+
+
+}
