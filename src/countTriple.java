ad5e5dc1828dde8e8c9fd5cc0497272d2df26f69
alzahara <shahdaljabri33@gmail.com>
2026-04-22 09:23:31 +0400

    add task 5

---
+++ src/countTriple.java
@@ -0,0 +1,24 @@
+public class countTriple {
+    public  static void mian (String[] args){
+        System.out.println(countTriple ("abcXXXabc"));
+
+    }
+
+
+    public static int countTriple (String str){
+        int x= str.length();
+        int count =0;
+
+        for (int i= 0; i< x-2 ; i++){
+            char tmp = str.charAt(i);
+        if (tmp ==str.charAt(i+1) && tmp == str.charAt(i+2))
+        count++;
+
+
+
+        }
+
+        return count;
+
+    }
+}
